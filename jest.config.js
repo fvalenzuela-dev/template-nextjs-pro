d4b3a14588a376ee9be109a0b41ca0eabd6c3e8c
kuroro <fernandovalenzuelaibarra@gmail.com>
2026-02-21 11:07:01 -0300

    mejoras

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,12 @@ module.exports = {
   coverageDirectory: "coverage",
   coverageReporters: ["lcov", "text"],
   collectCoverageFrom: ["src/utils/*.js"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
