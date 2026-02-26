77369cac5cc298f18791241561e94b52037e8c7a
Fernando <66434143+kuroroluzbell@users.noreply.github.com>
2026-02-26 16:12:28 -0300

    Modify sum function to return sum without multiplication

--- src/utils/sum.js
+++ src/utils/sum.js
@@ -1,11 +1,6 @@
-/**
- * Calculates the sum of two numbers and multiplies the result by two.
- * @param {number} a
- * @param {number} b
- * @returns {number} Double the sum of a and b.
- */
+
 function sum(a, b) {
-  return (a + b)*2;
+  return (a + b);
 }
 
 module.exports = sum;
